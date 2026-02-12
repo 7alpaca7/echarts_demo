34020451f7a6d9648eb2fb776c87301412f17125
alpaca <1460797554@qq.com>
2026-02-12 21:40:08 +0800

    部署尝试

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/echartsDemo/",
+  base:"/echarts_demo/",
   plugins: [
     vue(),
     // vueDevTools(),
